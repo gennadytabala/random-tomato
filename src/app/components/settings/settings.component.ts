190340ac10232806002879f7ef264944af5341f2
gennadytabala <45870843+gennadytabala@users.noreply.github.com>
2022-09-18 20:19:11 +0300

    settings form builder added

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -10,9 +10,12 @@ export class SettingsComponent implements OnInit {
 
   settingsForm: FormGroup | null = null
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm()
   }
 
   onSubmit() {
@@ -21,8 +24,31 @@ export class SettingsComponent implements OnInit {
 
   isFormValid() {
     console.log("isFormValid");
-    
     return true
+  }
+
+  buildForm() {
+    const form = {
+      workTimePercent: new FormControl({},Validators.required),
+      restTimePercent: new FormControl({},Validators.required),
+      maxSessionTime: new FormControl({},Validators.required), 
+    }
+    const formState = {
+      workTimePercent: {
+        value: 80,
+        disabled:false
+      },
+      restTimePercent: {
+        value: 20,
+        disabled:false
+      },
+      maxSessionTime: {
+        value: 30,
+        disabled:false
+      },
+    }
+    this.settingsForm = this.formBuilder.group(form)
+    this.settingsForm.reset()
   }
 
 }
