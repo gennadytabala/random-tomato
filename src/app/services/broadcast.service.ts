88181b02b9da3b703a035bb01e5c397aa3078633
gennadytabala <45870843+gennadytabala@users.noreply.github.com>
2022-09-18 21:08:30 +0300

    broadcast service added

---
+++ src/app/services/broadcast.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable, filter, map } from 'rxjs';
+
+export enum EventKeys {
+  ALL,
+  SETTINGS_BUTTON_CLICKED,
+  SETTINGS_SAVE_CLICKED,
+  SETTINGS_CHANGED,
+  TIME_TICK,
+  PROGRESS_INCREASED,
+  PROGRESS_DONE,
+  SESSION_CHANGED 
+}
+
+export interface IBroadcastEvent {
+  key: EventKeys
+  data?: any
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BroadcastService {
+
+  private eventBus = new Subject<IBroadcastEvent>()
+  
+  on(key:EventKeys): Observable<any>{
+    return this.eventBus.asObservable().pipe(
+      filter(event => event.key === EventKeys.ALL || event.key === key),
+      map(event => event.data)
+    )
+  }
+  
+  broadcast(key:EventKeys, data: any){
+    this.eventBus.next({key, data})
+  }
+
+}
