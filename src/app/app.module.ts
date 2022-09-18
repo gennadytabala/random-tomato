9a9b289d7ed1cd9f69ee38d52a9217673414d798
gennadytabala <45870843+gennadytabala@users.noreply.github.com>
2022-09-18 17:12:11 +0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SettingsComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
